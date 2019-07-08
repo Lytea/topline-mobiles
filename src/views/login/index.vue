988763437cd72efd6730f771529a93f4c2bc4f5d
‘Lytea <Lytea@qq.com>
2019-07-08 23:30:02 +0800

    登录请求数据

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <form>
     <van-nav-bar title="登录"/>
     <van-cell-group>
     <van-field
@@ -9,7 +10,6 @@
         label="用户名"
         placeholder="请输入用户名"
     />
-
     <van-field
         v-model="user.code"
         type="password"
@@ -18,10 +18,16 @@
         required
     />
     </van-cell-group>
+    <div class="login_btn">
+      <van-button type="info" class="btn" @click.prevent="handleLogin">登录</van-button>
+    </div>
+    </form>
   </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
+  name: 'LoginIndex',
   data () {
     return {
       user: {
@@ -29,9 +35,22 @@ export default {
         code: ''
       }
     }
+  },
+  methods: {
+    async handleLogin () {
+      const res = await axios({
+        method: 'POST',
+        url: 'http://toutiao.course.itcast.cn/app/v1_0/authorizations',
+        data: this.user
+      })
+      console.log(res)
+    }
   }
 }
 </script>
 <style lang="less" scoped>
-
+.btn {
+    width: 100%;
+    text-align: center;
+}
 </style>
