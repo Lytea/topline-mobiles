d05ea7edc9832f3a63fdfcf6cc6d12463f3262e6
Lytea <Lytea@qq.com>
2019-07-19 15:31:00 +0800

    配置dayjs日期处理库&&定义过滤器处理日期格式化

--- src/main.js
+++ src/main.js
@@ -9,7 +9,23 @@ import './styles/index.less'
 // 表单验证
 import zhCN from 'vee-validate/dist/locale/zh_CN'
 import VeeValidate, { Validator } from 'vee-validate'
-
+// 配置相对时间
+import 'dayjs/locale/zh-cn' // 加载中文语言包
+// dayjs相对时间插件
+import relativeTime from 'dayjs/plugin/relativeTime'
+import dayjs from 'dayjs'
+dayjs.extend(relativeTime) // 把插件注册到dayjs中
+dayjs.locale('zh-cn') // 配置使用中文语言包
+/**
+ * 注册一个全局过滤器来处理日期格式的显示
+ * 过滤器就是一个函数，我们可以再模板中通过 {{ 数据 | 过滤器 }}来调用这个过滤器函数
+ * 过滤器函数接收的参数就是你的数据，返回值就会绑定输出到使用的位置
+ * 它是一个全局函数，任何组件都可以去使用这个定义的过滤器函数
+ */
+Vue.filter('relativeTime', value => {
+  return dayjs().from(value)
+  // return 123
+})
 Vue.use(VeeValidate, {
   // 配置改变的时候去触发校验，默认是input
   events: ''
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -54,7 +54,7 @@
                 <p>
                   <span>{{ articleItem.aut_name }}</span>&nbsp;
                   <span>{{ articleItem.comm_count}}评论</span>&nbsp;
-                  <span>{{ articleItem.pubdate}}</span>
+                  <span>{{ articleItem.pubdate | relativeTime}}</span>
                 </p>
               </div>
               </van-cell>
