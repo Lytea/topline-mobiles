d1d5f5ee847bc767813dbebe89e35090374b1c18
Lytea <Lytea@qq.com>
2019-07-21 00:42:24 +0800

    展示结果数据

--- src/api/search.js
+++ src/api/search.js
@@ -11,3 +11,21 @@ export const getSuggestion = q => {
     }
   })
 }
+/**
+ * 获取搜索结果
+ */
+export const getSearchResult = ({
+  page, // 页码，不传为1
+  perPage, // 每页数量
+  q
+}) => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/search',
+    params: {
+      page,
+      per_page: perPage,
+      q
+    }
+  })
+}
--- src/views/search-result/index.vue
+++ src/views/search-result/index.vue
@@ -18,39 +18,54 @@
       @load="onLoad"
       >
       <van-cell
-          v-for="item in list"
-          :key="item"
-          :title="item"
+          v-for="item in searchArticle"
+          :key="item.art_id.toString()"
+          :title="item.title"
       />
     </van-list>
     <!-- /下拉列表 -->
   </div>
 </template>
 <script>
+import { getSearchResult } from '@/api/search'
 export default {
   name: 'searchResult',
   data () {
     return {
-      list: [],
+      searchArticle: [],
       loading: false,
-      finished: false
+      finished: false,
+      page: 1,
+      perPage: 20
+    }
+  },
+  computed: {
+    q () {
+      return this.$route.params.q
     }
   },
   methods: {
-    onLoad () {
-      // 异步更新数据
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
-        }
-        // 加载状态结束
+    async onLoad () {
+      await this.$sleep(800)
+      const data = await getSearchResult({
+        page: this.page,
+        perPage: this.perPage,
+        q: this.q
+      })
+      this.searchArticle = data.results
+      // 如果没有数据
+      if (!data.results.length) {
         this.loading = false
-
-        // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true
-        }
-      }, 500)
+        // 设置数据已加载结束
+        this.finished = true
+        return
+      }
+      // 如果有数据，将数据push到数组中加载更多
+      this.searchArticle.push(...data.results)
+      // 更新页码
+      this.page += 1
+      // 本地数据加载完毕，关闭loading(它每次onload的时候都会自动把onload设置为true)
+      this.loading = false
     }
   }
 }
