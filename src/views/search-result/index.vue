b1caa03c5f2d090d7d33eb1afdfbb0c5674998e7
Lytea <Lytea@qq.com>
2019-07-20 23:18:18 +0800

    完成搜索结果页面

--- src/views/search-result/index.vue
+++ src/views/search-result/index.vue
@@ -1,14 +1,62 @@
 <template>
-  <div>search page{{ $route.params.q}}</div>
+  <div>
+    <!-- 导航栏 -->
+    <van-nav-bar
+      title="搜索结果"
+      left-text="返回"
+      left-arrow
+      fixed
+      @click-left="$router.back()"
+    />
+    <!-- /导航栏 -->
+    <!-- 下拉列表 -->
+    <van-list
+      class="article-list"
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+      >
+      <van-cell
+          v-for="item in list"
+          :key="item"
+          :title="item"
+      />
+    </van-list>
+    <!-- /下拉列表 -->
+  </div>
 </template>
 <script>
 export default {
   name: 'searchResult',
   data () {
-    return {}
+    return {
+      list: [],
+      loading: false,
+      finished: false
+    }
+  },
+  methods: {
+    onLoad () {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 500)
+    }
   }
 }
 </script>
 <style lang="less" scoped>
-
+.article-list {
+  margin-top: 92px;
+}
 </style>
